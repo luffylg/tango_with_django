56379288757a5b4dd7a6e461820cccd25cf8801c
40huo <git@40huo.cn>
2017-08-27 21:50:18 +0800

    bootstraping rango

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
-    url(r'^accounts/register/$', MyRegistrationView.as_view, name='registration_register'),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
